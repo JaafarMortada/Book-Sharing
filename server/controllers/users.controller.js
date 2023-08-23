ed80fd5be8fe02f3f4c2432ddd2c80911dd2eeb7
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-23 17:51:13 +0300

    Server <> Minor Fixes

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,9 +1,15 @@
 const bcrypt = require("bcrypt")
 const User = require("../models/user.model")
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const { email, password, name } = req.body
 
+  const is_user_existing = await User.findOne({ email });
+
+  if (is_user_existing) {
+    return res.send({ message: "Already have an account" });
+  }
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = new User({
@@ -14,7 +20,13 @@ const register = async (req, res) => {
 
   try {
     await user.save();
-    res.send(user);
+    const { password: hashedPassword, ...userInfo } = user.toObject();
+    const token = jwt.sign(userInfo, process.env.SECRET_KEY);
+
+    return res.send({
+      token,
+      user: userInfo
+    });
   } catch (error) {
     console.error('Error creating user:', error);
     res.status(500).send('An error occurred while creating the user: ' + error.message);
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const app = express();
+const cors = require('cors');
+
 require("dotenv").config()
 
 const mongoDb = require("./configs/mongodb.connection");
 
 app.use(express.json())
-
+app.use(cors());
 const usersRoutes = require("./routes/users.route")
 app.use("/users", usersRoutes)
 
