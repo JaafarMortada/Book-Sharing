21f086fdd2e813782d2ba05451221efd1ca8478f
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-25 05:48:33 +0300

    Client <> Adding unLike Post API

--- client/src/components/bookCard/index.jsx
+++ client/src/components/bookCard/index.jsx
@@ -7,16 +7,21 @@ import { useEffect, useState } from 'react';
 const BookCard = ({data}) => {
 
     const [liked, setLiked] = useState(data.post.is_liked)
+    const [likesCount, setLikesCount] = useState(data.post.likes_count)
     const [following, setFollowing] = useState(data.post.is_following)
 
     const likeRequest = async () => {
         try {
             const response = await sendRequest({
                 method: "POST",
-                route: `/users/${data.post._id}/like`,
+                route: `/users/${data.post._id}/${!liked ? "like" : "un_like"}`,
             });
             if (response.message === "liked successfully"){
                 setLiked(true)
+                setLikesCount(likesCount+1)
+            } else if (response.message === "unLiked successfully"){
+                setLiked(false)
+                setLikesCount(likesCount-1)
             }
         } catch (error) {
             console.log(error)
@@ -57,7 +62,7 @@ const BookCard = ({data}) => {
             <div id="container">
                 <div className="product-details">
                     <div className='author-div'>
-                        <span><strong>Recommended by:</strong>{localStorage.getItem('name') === data.user_name ? "You": data.user_name}</span>
+                        <span><strong>Recommended by: </strong>{localStorage.getItem('name') === data.user_name ? "You": data.user_name}</span>
                         { localStorage.getItem('name') === data.user_name ? null :
                             <span 
                             className= {`follow-btn ${ following ? "follow-btn-following" : ""}`}
@@ -69,14 +74,19 @@ const BookCard = ({data}) => {
                     <h1>{data.post.title}</h1>
                     <div className="author-like-btn">
                         <span><strong>Author: </strong>{data.post.author}</span>
-                        { localStorage.getItem('name') === data.user_name ? null :
-                            <span 
+
+                        <div className='likes-container'>
+                            <span>{likesCount} Likes </span>
+                            { localStorage.getItem('name') === data.user_name ? null :
+                            <button 
                                 className={`like-btn ${ liked ? "like-button-red" : ""}`}
                                 onClick={likeRequest}
                             >
                                 <BiSolidLike/>
-                            </span>
+                            </button>
                         }
+                        </div>
+                        
                     </div>
                 </div>
                 <div class="product-image">
